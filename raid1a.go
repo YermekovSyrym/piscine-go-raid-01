62ddb0fea692462e81e73fb0cc4a2250b75a1bcc
YermekovSyrym <53341692+YermekovSyrym@users.noreply.github.com>
2019-10-19 17:08:04 +0600

    Update raid1a.go

--- raid1a.go
+++ raid1a.go
@@ -2,34 +2,34 @@ package student
 
 import "github.com/01-edu/z01"
 
-func Raid1a(x,y int) {
+func Raid1a(x, y int) {
 
 	if x > 0 && y > 0 {
 		//draw top width
 		z01.PrintRune('o')
-		for a := 0; a < x-2; a++ {		
-			z01.PrintRune('-')	
+		for a := 0; a < x-2; a++ {
+			z01.PrintRune('-')
 		}
 		if x > 1 {
 			z01.PrintRune('o')
 		}
-			z01.PrintRune('\n')
-			//draw length
-			for b := 0; b < y-2; b++ {
-				z01.PrintRune('|')
-				for c := 0; c < x-2; c++ {
-					z01.PrintRune(' ')
-				}
-				if x > 1 {
-					z01.PrintRune('|')
-				}
-				z01.PrintRune('\n')
+		z01.PrintRune('\n')
+		//draw length
+		for b := 0; b < y-2; b++ {
+			z01.PrintRune('|')
+			for c := 0; c < x-2; c++ {
+				z01.PrintRune(' ')
 			}
-			//draw bottom width
+			if x > 1 {
+				z01.PrintRune('|')
+			}
+			z01.PrintRune('\n')
+		}
+		//draw bottom width
 		if y > 1 {
 			z01.PrintRune('o')
-			for a := 0; a < x-2; a++ {		
-				z01.PrintRune('-')	
+			for a := 0; a < x-2; a++ {
+				z01.PrintRune('-')
 			}
 			if x > 1 {
 				z01.PrintRune('o')
